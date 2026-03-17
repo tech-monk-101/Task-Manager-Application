060377bc95ab701eec1d73196f763fe502387391
Manjunadh <kasammanjunadh@gmail.com>
2026-03-17 16:22:59 +0530

    Refactored TaskController To integrate with database Also implemented some custom queries (TaskRepository, TaskController)

--- src/main/java/com/techmonk/taskmanager/controller/TaskController.java
+++ src/main/java/com/techmonk/taskmanager/controller/TaskController.java
@@ -1,10 +1,12 @@
 package com.techmonk.taskmanager.controller;
 
 import com.techmonk.taskmanager.entity.Task;
+import com.techmonk.taskmanager.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController //Combination of @Controller and @ResponseBody
@@ -13,57 +15,71 @@ import java.util.List;
 //localhost:8080/api/v1/tasks
 public class TaskController {
 
-    //Temporary in memory task list, later replaced with database
-    private List<Task> tasks = new ArrayList<>();
-    private Long nextId = 1L;
+    //taskRepository instance gets injected here
+    private final TaskRepository taskRepository;
+
+    //Constructor injection
+    //Spring Boot automatically handles constructor injection by below constructor.
+    //The singleton instance : taskRepository bean gets injected to task controller using constructor injection
+    @Autowired
+    public TaskController(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
 
     //Get endpoint which retrieves and returns all tasks
     @GetMapping
     public List<Task> getAllTasks() {
-
-        return tasks;
+        return taskRepository.findAll(); //function from parent class JpaRepository
     }
 
     //Get endpoint which retrieves and returns task by id, specified by path variable
     @GetMapping("/{id}")
     //@PathVariable extracts the variable id from the url
-    public Task getTaskById(@PathVariable Long id) {
+    //ResponseEntity makes it easy to handle status codes
+    public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
         //Using Steams, can also use for loop method specified in Put Mapping
-        return tasks.stream()
-                .filter(task -> task.getId().equals(id))
-                .findFirst()
-                .orElse(null);
+        return taskRepository.findById(id) //function from JpaRepository
+                .map(ResponseEntity::ok) //If found it's wrapped with response entity with status code ok
+                .orElse(ResponseEntity.notFound().build()); //if not found
     }
 
     @PutMapping("/{id}")
-    public Task updateTask(@PathVariable Long id, @RequestBody Task updatedTask) {
-        for(int i = 0; i < tasks.size(); i++) {
-            Task task = tasks.get(i);
-            if(task.getId().equals(id)) {
-                updatedTask.setId(id);
-                updatedTask.setCreatedAt(task.getCreatedAt());
-                updatedTask.setCompleted(task.getCompleted());
-                tasks.set(i, updatedTask);
-                return updatedTask;
-            }
-        }
-        return null;
+    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task updatedTask) {
+        return taskRepository.findById(id)
+                .map(task -> {
+                    task.setTitle(updatedTask.getTitle());
+                    task.setDescription(updatedTask.getDescription());
+                    task.setCompleted(updatedTask.getCompleted());
+                    Task savedTask = taskRepository.save(task);
+                    return ResponseEntity.ok(savedTask);
+                })
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping //Handles HTTP Post Requests
     //@RequestBody tells spring to convert the JSON in the Request body to an object (JSON Parsing)
-    public Task createTask(@RequestBody Task task) {
-        task.setId(nextId++);
-        task.setCreatedAt(LocalDateTime.now());
-        task.setCompleted(false);
-        tasks.add(task);
-
-        return task;
+    public ResponseEntity<Task> createTask(@RequestBody Task task) {
+        Task savedTask = taskRepository.save(task); //JpaRepository function
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedTask);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteTask(@PathVariable Long id) {
-        tasks.removeIf(task -> task.getId().equals(id));
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        return taskRepository.findById(id)
+                .map(task -> {
+                    taskRepository.delete(task);
+                    return ResponseEntity.noContent().<Void>build();
+                })
+                .orElse(ResponseEntity.notFound().build());
     }
 
+    @GetMapping("/completed/{status}")
+    public List<Task> getTasksByCompletions(@PathVariable boolean status) {
+        return taskRepository.findByCompleted(status);
+    }
+
+    @GetMapping("/search")
+    public List<Task> searchTasksByTitle(@RequestParam String title) {
+        return taskRepository.findByTitleContainingIgnoreCase(title);
+    }
 }
--- src/main/java/com/techmonk/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/techmonk/taskmanager/repository/TaskRepository.java
@@ -2,9 +2,25 @@ package com.techmonk.taskmanager.repository;
 
 import com.techmonk.taskmanager.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.List;
+
+//@Repository - Not Required as JpaRepository Already Has It
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    //Method Name Query
+    //SELECT * FROM tasks WHERE completed = :completed
+    List<Task> findByCompleted(boolean completed);
+
+    List<Task> findByTitleContainingIgnoreCase(String title);
+
+    //JPQL QUERY?? - JAVA PERSISTENCE QUERY LANGUAGE
+    //LIKE SQL BUT USES ENTITY NAMES INSTEAD OF TABLE NAMES
+    @Query("SELECT t FROM Task t where t.completed = :completed")
+    List<Task> findTaskByCompletionStatus(@Param("completed") boolean completed);
 }
 //Spring Magic: Spring Data is going to auto generate an implementation for save, findall, findbyid, etc.
+//Interface can be left empty, if you want custom queries, write that methods in it
